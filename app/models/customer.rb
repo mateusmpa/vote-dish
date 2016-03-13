07e19da4da9af29745912365d04fd6b7af7f5a9f
Mateus Alves <mateusmpa@gmail.com>
2016-03-13 18:43:41 -0300

    add validates to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ActiveRecord::Base
+  validates_presence_of :name, message: ' - must be filled'
+  validates_uniqueness_of :name, message: ' - already registered name'
+  validates_numericality_of :age, greater_than: 0, less_than: 100, message: 'number must be between 0 and 100'
 end
