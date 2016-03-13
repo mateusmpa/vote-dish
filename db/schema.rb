a4ec9b96f86725e09565347b0032b91a38222333
Mateus Alves <mateusmpa@gmail.com>
2016-03-13 18:50:38 -0300

    model dish

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160313213126) do
+ActiveRecord::Schema.define(version: 20160313214848) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 20160313213126) do
   create_table "customers", force: :cascade do |t|
     t.string   "name",       limit: 80
     t.integer  "age"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+  end
+
+  create_table "dishes", force: :cascade do |t|
+    t.string   "name",       limit: 80
     t.datetime "created_at",            null: false
     t.datetime "updated_at",            null: false
   end
