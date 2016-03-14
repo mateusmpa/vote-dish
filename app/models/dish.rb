c84f6fd914fc7d4765027b843b484d675e97fe8e
Mateus Alves <mateusmpa@gmail.com>
2016-03-13 21:00:22 -0300

    validates of relationships

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,6 +2,13 @@ class Dish < ActiveRecord::Base
   validates_presence_of :name, message: ' - must be filled'
   validates_uniqueness_of :name, message: ' - already registered name'
 
-  has_and_belongs_to_many :restaurant
+  validate :validate_presence_of_more_than_one_restaurant
+
+  has_and_belongs_to_many :restaurants
   has_one :recipe
+
+  private
+  def validate_presence_of_more_than_one_restaurant
+    errors.add(:restaurants, 'must be at least one restaurant') if restaurants.empty?
+  end
 end
--- app/models/qualification.rb
+++ app/models/qualification.rb
@@ -2,8 +2,11 @@ class Qualification < ActiveRecord::Base
   validates_presence_of :note, message: ' - must be filled'
   validates_presence_of :amount_spent, message: ' - must be filled'
 
-  validates_nmericality_of :note, greater_than_or_equal_to: 0, less_than_or_equal_to: 10, message: 'number must be between 0 and 10'
+  validates_numericality_of :note, greater_than_or_equal_to: 0, less_than_or_equal_to: 10, message: 'number must be between 0 and 10'
   validates_numericality_of :amount_spent, greater_than: 0, message: ' - number must be greater than 0'
+
+  validates_presence_of :customer_id, :restaurant_id
+  validates_associated :customer, :restaurant
 
   belongs_to :customer
   belongs_to :restaurant
