a69ae17102010ac2d22a9c9a5bce42c4db29e303
Mateus Alves <mateusmpa@gmail.com>
2016-03-25 02:37:35 -0300

    remove default url from restaurant

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,5 +1,5 @@
 class Restaurant < ActiveRecord::Base
-  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }, default_url: '/images/:style/missing.png'
+  has_attached_file :avatar, styles: { medium: '300x300>', thumb: '100x100>' }
   validates_attachment_content_type :avatar, content_type: ['image/jpeg', 'image/png', 'image/gif']
 
   validates_presence_of :name, message: 'must be filled'
